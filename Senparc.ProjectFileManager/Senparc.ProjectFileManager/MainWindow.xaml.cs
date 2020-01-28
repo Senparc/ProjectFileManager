326f26a021b01e3dcac8cd988b7c14befc5b3ae6
苏震巍 <zsu@senparc.com>
2020-01-28 23:42:13 +0800

    完成基础界面和部分基础方法

---
+++ Senparc.ProjectFileManager/Senparc.ProjectFileManager/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Senparc.CO2NET.Trace;
+using Senparc.ProjectFileManager.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Senparc.ProjectFileManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PropertyGroup SelectedFile { get; set; } = new PropertyGroup() { FullFilePath = "[ no file selectd ]" };
+        private List<PropertyGroup> ProjectFiles { get; set; } = new List<PropertyGroup>();
+        private List<XDocument> ProjectDocuments { get; set; } = new List<XDocument>(); 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SenparcTrace.SendCustomLog("System", "Window opened.");
+
+            //lblFilePath.DataContext = SelectedFile;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var path = txtPath.Text?.Trim();
+            if (string.IsNullOrEmpty(path) || !Directory.Exists(path))
+            {
+                MessageBox.Show("Please input the correct path which includes .csproj files！", "error");
+            }
+
+            SenparcTrace.SendCustomLog("Task", "Search .csproj files begin.");
+
+            var csprojFiles = Directory.GetFiles(path, "*.csproj", SearchOption.AllDirectories);
+            foreach (var file in csprojFiles)
+            {
+                try
+                {
+                    var doc = XDocument.Load(file);
+                    var propertyGroup = doc.Root.Elements("PropertyGroup").FirstOrDefault();
+                    if (propertyGroup==null)
+                    {
+                        throw new Exception($"{file} is not a valid xml.csproj file.");
+                    }
+
+                    var projectFile = PropertyGroup.GetObjet(propertyGroup, file);
+                    ProjectFiles.Add(projectFile);
+                    ProjectDocuments.Add(doc);
+
+                    SenparcTrace.SendCustomLog("Task", $"[Success] Load file:{file}");
+                }
+                catch (Exception ex)
+                {
+                    SenparcTrace.SendCustomLog("Task", $"[Faild] Load file:{file}");
+                    SenparcTrace.SendCustomLog("Error", ex.Message);
+                }
+            }
+
+            Dictionary<string, string> lbFilesData = new Dictionary<string, string>();
+            foreach (var projectFile in ProjectFiles)
+            {
+                lbFilesData[projectFile.FileName] = projectFile.FullFilePath;
+            }
+
+            lbFiles.ItemsSource = lbFilesData;
+        }
+
+    }
+}
---
+++ Senparc.ProjectFileManager/Senparc.ProjectFileManager/Models/PropertyGroup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Senparc.ProjectFileManager.Models
+{
+    /// <summary>
+    /// .csproj file items in the first &lt;PropertyGroup&gt; tag
+    /// </summary>
+    public class PropertyGroup
+    {
+        public string TargetFrameworks { get; set; }
+        public string Version { get; set; }
+        public string AssemblyName { get; set; }
+        public string RootNamespace { get; set; }
+        public string Description { get; set; }
+        public string Copyright { get; set; }
+        public string PackageTags { get; set; }
+        public string Authors { get; set; }
+        public string Owners { get; set; }
+        public string PackageLicenseUrl { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string PackageProjectUrl { get; set; }
+        public string PackageIconUrl { get; set; }
+        public string PackageReleaseNotes { get; set; }
+        public string RepositoryUrl { get; set; }
+
+        #region 补充信息
+        public XElement OriginalElement { get; set; }
+
+        public string FileName => Path.GetFileName(FullFilePath);
+        public string FullFilePath { get; set; }
+        #endregion
+
+        public static PropertyGroup GetObjet(XElement element,string fullFilePath)
+        {
+            var projectFile = Senparc.CO2NET.Utilities.XmlUtility.Deserialize<PropertyGroup>(element.ToString()) as PropertyGroup;
+            if (projectFile == null)
+            {
+                throw new Exception("invalid xml file for new .csproj file format!");
+            }
+            projectFile.OriginalElement = element;
+            projectFile.FullFilePath = fullFilePath;
+            return projectFile;
+        }
+    }
+}
