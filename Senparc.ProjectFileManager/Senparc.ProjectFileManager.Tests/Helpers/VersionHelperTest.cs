58c19928708eb7de5a44add22a104bff2a90fec5
苏震巍 <zsu@senparc.com>
2020-01-29 19:32:53 +0800

    add Test project

---
+++ Senparc.ProjectFileManager/Senparc.ProjectFileManager.Tests/Helpers/VersionHelperTest.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Senparc.ProjectFileManager.Helpers;
+
+namespace Senparc.ProjectFileManager.Tests.Helpers
+{
+    [TestClass]
+    public class VersionHelperTest
+    {
+        [TestMethod]
+        public void GetVersionObjectTest()
+        {
+            var versionStr = "1.0-preview2";
+            var versionObject = VersionHelper.GetVersionObject(versionStr);
+            Assert.AreEqual(versionStr, versionObject.ToString());
+
+            versionStr = "3.0.0.3";
+            versionObject = VersionHelper.GetVersionObject(versionStr);
+            Assert.AreEqual(versionStr, versionObject.ToString());
+
+            versionStr = "3.0.0.0-preview3";
+            versionObject = VersionHelper.GetVersionObject(versionStr);
+            Assert.AreEqual("3.0-preview3", versionObject.ToString());//just keep 2 segments
+
+
+        }
+    }
+}
